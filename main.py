d9cd8281b3c02acdd8f285ceacc38ca43372b784
Phyb1 <phibeon@yahoo.com>
2022-05-26 11:14:48 +0200

    'finding=anagrams'

---
+++ main.py
@@ -0,0 +1,18 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    if len(word) == len(anagram):
+        for i in word:
+            if i not in anagram:                
+                return False
+        return True
+    else:
+       return False 
+
+print(find_anagram("below", "elbow"))  
+
+
